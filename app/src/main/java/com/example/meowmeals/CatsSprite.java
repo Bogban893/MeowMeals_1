ae196fa20f0cab37beae6060a2186380de6523ea
Bogban893 <bogbanrvd@gmail.com>
2024-04-06 14:23:01 +0300

    commit

---
+++ app/src/main/java/com/example/meowmeals/CatsSprite.java
@@ -0,0 +1,20 @@
+package com.example.meowmeals;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Rect;
+
+public class CatsSprite extends Sprite{
+    private int x,y,Vx,Vy;
+    private Bitmap bitmap;
+    Paint paint = new Paint();
+
+    public CatsSprite(int x, int y, int Vx, int Vy,Bitmap b){
+        super(x,y,Vx,Vy,b);
+        this.bitmap = b;
+    }
+    public void draw(Canvas canvas){canvas.drawBitmap(bitmap, x, y-bitmap.getHeight(), paint);}
+
+    public void getRect(){return new Rect(this.x,this.y-bitmap.getHeight(),this.x+bitmap.getWidth(),this.y);}
+}
